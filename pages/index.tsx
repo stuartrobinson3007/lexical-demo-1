d8c016567b45b8428401f1d784f57ee641742bfa
Stuart <stuartrobinson3007@gmail.com>
2022-06-27 12:38:58 -0500

    .

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,29 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 
+const blogData = [
+  {
+    title: 'Blog title 1',
+    summary: 'Blog summary goes here'
+  },
+  {
+    title: 'Blog title 2',
+    summary: 'Blog summary goes here'
+  },
+  {
+    title: 'Blog title 3',
+    summary: 'Blog summary goes here'
+  },
+  {
+    title: 'Blog title 4',
+    summary: 'Blog summary goes here'
+  },
+  {
+    title: 'Blog title 5',
+    summary: 'Blog summary goes here'
+  },
+]
+
 const Home: NextPage = () => {
   const pageTitle = "Stuart Robinson | Home";
   const pageDescription = "Meta description.";
@@ -17,8 +40,17 @@ const Home: NextPage = () => {
         <meta name="keywords" content="keyword, keyword" />
       </Head>
 
-      <div className='mt-12'>
-        Hello world
+      <div className='mt-40 max-w-4xl m-auto'>
+        <div className='grid grid-cols-3 gap-16'>
+          {blogData.map((data, index) => {
+            return (
+              <div className='border-2 border-slate-400 rounded p-4' key={index}>
+                <div className='font-bold'>{data.title}</div>
+                {data.summary}
+              </div>
+            )
+          })}
+        </div>
       </div>
 
     </div>
